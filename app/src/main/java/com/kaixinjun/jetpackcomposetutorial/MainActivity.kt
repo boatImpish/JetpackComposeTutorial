39679b9867b54eda51b294b192c6f00627a0eaa0
liusheng <liusheng@swhysc.com>
2023-11-07 09:26:28 +0800

    1, add multiple text; 2,Using a column and a row; 3, Add an image element

--- app/src/main/java/com/kaixinjun/jetpackcomposetutorial/MainActivity.kt
+++ app/src/main/java/com/kaixinjun/jetpackcomposetutorial/MainActivity.kt
@@ -3,15 +3,19 @@ package com.kaixinjun.jetpackcomposetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard(name = "World")
+            PreviewMessageCard()
         }
     }
 }
@@ -20,8 +24,16 @@ data class Message(val author: String, val body: String)
 
 @Composable
 fun MessageCard(msg: Message) {
-    Text(text = msg.author)
-    Text(text = msg.body)
+    Row {
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_foreground),
+            contentDescription = "Contact profile picture"
+        )
+        Column {
+            Text(text = msg.author)
+            Text(text = msg.body)
+        }
+    }
 }
 
 @Preview
